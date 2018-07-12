fe4a2858ed4588ea8faa91bbcfe5ab0cc0bfb0eb
unknown <ecorisepune@gmail.com>
2018-07-12 10:44:48 +0530

    app.js added

---
+++ app/app.js
@@ -0,0 +1,78 @@
+var app=angular.module('ecojobs', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl : "templates/home.html"
+	})
+	.when('/about-us', {
+		templateUrl:"templates/about-us.html";
+	})
+	.when('/account-forgot-password-page', {
+		templateUrl:"templates/account-forgot-password-page.html";
+	})
+	.when('/account-login-page', {
+		templateUrl:"templates/account-login-page.html";
+	})
+	.when('/account-register-page', {
+		templateUrl:"templates/account-register-page.html";
+	})
+	.when('/admin-change-pass', {
+		templateUrl:"templates/admin-change-pass.html";
+	})
+	.when('/admin-empty', {
+		templateUrl:"templates/admin-empty.html";
+	})
+	.when('/admin-profile', {
+		templateUrl:"templates/admin-profile.html";
+	})
+	.when('/admin-saved-job', {
+		templateUrl:"templates/aadmin-saved-job.html";
+	})
+	.when('/admin', {
+		templateUrl:"templates/admin.html";
+	})
+	.when('/blog-single', {
+		templateUrl:"templates/blog-single.html";
+	})
+	.when('/blog', {
+		templateUrl:"templates/blog.html";
+	})
+	.when('/contact', {
+		templateUrl:"templates/contact.html";
+	})
+	.when('/employee-create-resume', {
+		templateUrl:"templates/employee-create-resume.html";
+	})
+	.when('/employee-detail', {
+		templateUrl:"templates/employee-detail.html";
+	})
+	.when('/employee', {
+		templateUrl:"templates/employee.html";
+	})
+	.when('/employer-detail', {
+		templateUrl:"templates/employer-detail.html";
+	})
+	.when('/account-forgot-password-page', {
+		templateUrl:"templates/account-forgot-password-page.html";
+	})
+	.when('/faq', {
+		templateUrl:"templates/faq.html";
+	})
+	.when('/job-browse-job', {
+		templateUrl:"templates/job-browse-job.html";
+	})
+	.when('/job-category', {
+		templateUrl:"templates/job-category.html";
+	})
+	.when('/job-detail', {
+		templateUrl:"templates/job-detail.html";
+	})
+	.when('/job-location', {
+		templateUrl:"templates/job-location.html";
+	})
+	.when('/job-result', {
+		templateUrl:"templates/job-result.html";
+	});
+ 
+});
